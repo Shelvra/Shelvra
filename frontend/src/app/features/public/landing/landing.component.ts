2636a0cada7658e0219652c4d7b37a1ec0d7f3ba
Duc Hoang <minhduc8199@gmail.com>
2025-03-15 18:58:15 +0100

    add langind page background

--- frontend/src/app/features/public/landing/landing.component.ts
+++ frontend/src/app/features/public/landing/landing.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-landing',
+  selector: 'app-landing-page',
   imports: [],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
