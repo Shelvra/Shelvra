8bc62aa7310937f903d42662c9538df89eeec3c6
Hannah Anderson <andersonhan21@gmail.com>
2025-03-20 13:55:49 +0100

    Added folder for landing page and header bar
    Beginning to create landing page

---
+++ frontend/src/app/landing/headerbar/headerbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headerbar',
+  imports: [],
+  templateUrl: './headerbar.component.html',
+  styleUrl: './headerbar.component.scss'
+})
+export class HeaderbarComponent {
+
+}
