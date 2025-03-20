e792b9aef392c9067ad142c87a98b9f38eede86f
Hannah Anderson <andersonhan21@gmail.com>
2025-03-20 15:52:15 +0100

    Added basic log in details

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderbarComponent } from './landing/headerbar/headerbar.component';
+import { LoginComponent } from './landing/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderbarComponent],
+  imports: [RouterOutlet, HeaderbarComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
