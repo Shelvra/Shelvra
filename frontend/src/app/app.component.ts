d1cc7ba5f5c24bd593ac2072ad4b7b073145fc3a
Hannah Anderson <andersonhan21@gmail.com>
2025-03-21 10:18:43 +0100

    Added tagline and arranged login components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderbarComponent } from './landing/headerbar/headerbar.component';
 import { LoginComponent } from './landing/login/login.component';
+import { TaglineComponent } from './landing/tagline/tagline.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderbarComponent, LoginComponent],
+  imports: [RouterOutlet, HeaderbarComponent, LoginComponent, TaglineComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ frontend/src/app/landing/tagline/tagline.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tagline',
+  imports: [],
+  templateUrl: './tagline.component.html',
+  styleUrl: './tagline.component.scss'
+})
+export class TaglineComponent {
+
+}
