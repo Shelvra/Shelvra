6c959a587e15d00bc72d7a81d1acbe6533afc621
Hannah Anderson <andersonhan21@gmail.com>
2025-03-21 16:37:27 +0100

    Initial demo setup

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderbarComponent } from './landing/headerbar/headerbar.component';
 import { LoginComponent } from './landing/login/login.component';
 import { TaglineComponent } from './landing/tagline/tagline.component';
+import { DemoComponent } from './landing/demo/demo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderbarComponent, LoginComponent, TaglineComponent],
+  imports: [RouterOutlet, HeaderbarComponent, LoginComponent, TaglineComponent, DemoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
